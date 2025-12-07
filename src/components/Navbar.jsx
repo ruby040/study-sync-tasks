6ee394b102006e6e926542f436bf3465ccfd1a7a
lulwah-mohammed <148808819+lulwah-mohammed@users.noreply.github.com>
2025-12-07 07:57:24 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.jsx";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig.js";
+import "../styles/navbar.css";
 
 function Navbar() {
   const { user } = useAuth();
