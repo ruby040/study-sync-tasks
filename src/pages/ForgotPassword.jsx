b8338f5aa8f91b8f95ca6106ab5b3f7d9939ec5f
lulwah-mohammed <148808819+lulwah-mohammed@users.noreply.github.com>
2025-12-07 07:58:36 +0300

    Update ForgotPassword.jsx

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig";
 import { Link } from "react-router-dom";
+import "../styles/auth.css";
 
 function ForgotPassword() {
   const [email, setEmail] = useState("");
@@ -23,28 +24,29 @@ function ForgotPassword() {
   };
 
   return (
-    <div>
-      <h1>Reset Password</h1>
+    <div className="auth-container">
+  <h1>Reset Password</h1>
 
-      {message && <p style={{ color: "green" }}>{message}</p>}
-      {error && <p style={{ color: "red" }}>{error}</p>}
+  {message && <p style={{ color: "green" }}>{message}</p>}
+  {error && <p style={{ color: "red" }}>{error}</p>}
 
-      <form onSubmit={handleReset}>
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
+  <form onSubmit={handleReset}>
+    <input
+      type="email"
+      placeholder="Enter your email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      required
+    />
 
-        <button type="submit">Send Reset Email</button>
-      </form>
+    <button type="submit">Send Reset Email</button>
+  </form>
 
-      <p>
-        <Link to="/login">Back to login</Link>
-      </p>
-    </div>
+  <p>
+    <Link to="/login">Back to login</Link>
+  </p>
+</div>
+
   );
 }
 
